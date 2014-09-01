28c2f0d521f4ed25c282bb961617ff8fdec68aa9
cassidypignatello <cassidy.pignatello@gmail.com>
2014-09-01 17:17:14 -0400

    finishes contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -39,4 +39,16 @@ describe "Static Pages" do
     end
   end
 
+  describe "Contact Page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Contact')
+    end
+  end
+
 end
